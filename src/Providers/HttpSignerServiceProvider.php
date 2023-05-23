c9e3a4ecdf778c335758e7680de434bf825f7fec
foxett <foxett@yandex.ru>
2023-05-23 19:30:12 +0300

    fix publish config

--- src/Providers/HttpSignerServiceProvider.php
+++ src/Providers/HttpSignerServiceProvider.php
@@ -8,12 +8,9 @@ class HttpSignerServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-
             $this->publishes([
                 __DIR__ . '/../../config/http-signer.php' => config_path('http-signer.php'),
-            ]);
+            ], 'http-signer-config');
 
-        }
     }
 }
